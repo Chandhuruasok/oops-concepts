d7f1ca62d39a1cfa1181a26f3a71e075e982bb42
Chan3562 <Chan3562@CSL0705.global.chainsys.com>
2024-04-25 13:07:43 +0530

    updated login page

---
+++ oops/src/com/oops/encap/CollegeAdmin.java
@@ -0,0 +1,143 @@
+package com.oops.encap;
+import java.util.*;
+public class CollegeAdmin {
+	public static void psnaAdmin()
+	{
+		Scanner s=new Scanner(System.in);
+		String adminNamePsna="Psna";
+		String adminPasswordPsna="Psna@1";
+		System.out.println("Enter the admin name:");
+		String adminName=s.next();
+		System.out.println("Enter the admin password:");
+		String adminPassword=s.next();
+		while(!(adminName.matches(adminNamePsna)))
+		{
+			System.out.println("Enter the correct admin name:");
+		    adminName=s.next();
+			
+		}
+		while(!adminPassword.matches(adminPasswordPsna))
+		{
+			System.out.println("Enter the correct admin password:");
+		    adminPassword=s.next();
+		}
+		System.out.println("---Welcome Admin---");
+		System.out.println("whether you need to add extra features(1.yes/2.no):");
+		int option=s.nextInt();
+		switch(option)
+		{
+		   case 1:
+			   System.out.println("---Features can be added---");
+			   System.out.println("select option to add features(1.Placement 2.Ranking)");
+			   int o=s.nextInt();
+			   switch(o)
+			   {
+			   case 1:
+				   System.out.println("please enter the current year overall placement percentage:");
+				   double percent=s.nextDouble();
+				   if(percent<0)
+				   {
+					   System.out.println("please enter the current year overall placement percentage:");
+					   percent=s.nextDouble();
+					  
+				   }
+				   System.out.println("current overall placement percentage of our college is:"+percent);
+				   System.out.println("---Thank you features are updated---");
+				   break;
+			   case 2:
+				   System.out.println("please enter the updated new ranking of the college:");
+				   int rank=s.nextInt();
+				   if(rank<0)
+				   {
+					   System.out.println("please enter the  updated new ranking of the college:");
+					   rank=s.nextInt();
+					  
+				   }
+				   System.out.println("current ranking of our college is:"+rank);
+				   System.out.println("---Thank you features are updated---");
+				   break;
+			   }
+			   break;
+		   case 2:
+			   System.out.println("---Thank you No features are added---");
+			   break;
+		}
+		
+		
+	}
+	public static void psnaUser()
+	{
+	
+	    Scanner scan=new Scanner(System.in);
+	    String userNamePsna="^[a-zA-Z]+$";
+		String userPasswordPsna="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8}$";
+	    System.out.println("Whether you are 1.new user 2.existing user ");
+	    int option=scan.nextInt();
+	    
+	    if(option==1)
+	    {
+	    	System.out.println("---SIGN UP PAGE---");
+	    	System.out.println("create userName:");
+	    	String userName1=scan.next();
+	    	while(!(userName1.matches(userNamePsna)))
+	    	{
+	    		System.out.println("create userName:");
+		    	 userName1=scan.next();
+	    	}
+	    	System.out.println("create userPassword:");
+	    	String userPassword1=scan.next();
+	    	while(!(userPassword1.matches(userPasswordPsna)))
+	    	{
+	    		System.out.println("create userPassword:");
+		    	 userPassword1=scan.next();
+	    	}
+	    	System.out.println("---SIGN UP SUCCESSFUL---");
+	    }
+	    else if(option==2)
+	    {
+	    	System.out.println("---LOGIN PAGE---");
+	    	System.out.println("Enter the user name:");
+	    	String userName2=scan.next();
+	
+	    	while(!(userName2.matches(userNamePsna)))
+	    	{
+	    		System.out.println("Enter the user name:");
+		    	 userName2=scan.next();
+	    	}
+	    	System.out.println("Enter the user password:");
+	    	 String userPassword2=scan.next();
+	    	 while(!(userPassword2.matches(userPasswordPsna)))
+		    	{
+		    		System.out.println("Enter the user name:");
+			    	 userName2=scan.next();
+		    	}
+	    	 System.out.println("---LOGIN SUCCESSFUL---");
+	    		
+	    }
+		
+		System.out.println("---Welcome User---");
+		System.out.println("enter your roll number:");
+		int rollNo=scan.nextInt();
+		if(rollNo<0)
+		{
+			System.out.println("Enter your attendance percentage:");
+			rollNo=scan.nextInt();
+		}
+		System.out.println("Enter your attendance percentage:");
+		int percent=scan.nextInt();
+		if(percent<0)
+		{
+			System.out.println("Enter your attendance percentage:");
+			 percent=scan.nextInt();
+		}
+		if(percent>70)
+		{
+			System.out.println("you are eligible to write the exam");
+		}
+		else
+		{
+			System.out.println("pay Rs.100 to attend the exam");
+		}
+		
+}
+}
--- oops/src/com/oops/encap/TestCollegeApp.java
+++ oops/src/com/oops/encap/TestCollegeApp.java
@@ -7,136 +7,17 @@ public class TestCollegeApp {
 		Scanner scan=new Scanner(System.in);
 		CollegeManagementApp cp=new CollegeManagementApp();
 		CollegeApp ca=new CollegeApp();
-		System.out.println("---1.PSNACET---");
-		System.out.println("---2.SSMIET---");
-		System.out.println("---3.PSG---");
-		System.out.println("---4.KCT---");
-		System.out.println("Choose the college  from the above option:");
-		int option=scan.nextInt();
-		if(option<0)
+		System.out.println("select 1.admin/ 2.user");
+		int selectOption=scan.nextInt();
+		switch(selectOption)
 		{
-			System.out.println("Choose the college  from the above option:");
-			option=scan.nextInt();
-		}
-		int select;
-		switch(option)
-		{
-		case 1:
-			System.out.println("---Welcome to PSNACET---");
-			System.out.println("College code is 5901");
-			System.out.println("1.Government quota \n 2.Management quota");
-			System.out.println("Enter the type of quota:");
-			select=scan.nextInt();
-			if(select<0)
-			{
-				System.out.println("Enter the type of quota:");
-				select=scan.nextInt();
-			}
-			switch(select)
-			{
 			case 1:
-				System.out.println("---Fees structure for Government quota---");
-				System.out.println("Fees structure for ECE is 1,00,000");
-				System.out.println("Fees structure for CSE is 1,50,000");
-				System.out.println("Fees structure for IT is  1,20,000");
-				System.out.println("Fees structure for MCA is 1,60,000");
-				break;
-			case 2:
-				System.out.println("---Fees structure for Management quota---");
-				System.out.println("Fees structure for ECE is 2,00,000");
-				System.out.println("Fees structure for CSE is 2,50,000");
-				System.out.println("Fees structure for IT is  2,20,000");
-				System.out.println("Fees structure for MCA is 2,60,000");
-				break;
-			}
+			CollegeAdmin.psnaAdmin();
 			break;
-		case 2:
-			System.out.println("---Welcome to SSMIET---");
-			System.out.println("College code is 5902");
-			System.out.println("1.Government quota \n 2.Management quota");
-			System.out.println("Enter the type of quota:");
-			select=scan.nextInt();
-			if(select<0)
-			{
-				System.out.println("Enter the type of quota:");
-				select=scan.nextInt();
-			}
-			switch(select)
-			{
-			case 1:
-				System.out.println("---Fees structure for Government quota---");
-				System.out.println("Fees structure for ECE is 50,000");
-				System.out.println("Fees structure for CSE is 52,000");
-				System.out.println("Fees structure for IT is  55,000");
-				System.out.println("Fees structure for MCA is 60,000");
-				break;
 			case 2:
-				System.out.println("---Fees structure for Management quota---");
-				System.out.println("Fees structure for ECE is 60,000");
-				System.out.println("Fees structure for CSE is 62,000");
-				System.out.println("Fees structure for IT is  65,000");
-				System.out.println("Fees structure for MCA is 70,000");
+				CollegeAdmin.psnaUser();
 				break;
-			}
-			break;
-		case 3:
-			System.out.println("---Welcome to PSG---");
-			System.out.println("College code is 5903");
-			System.out.println("1.Government quota \n 2.Management quota");
-			System.out.println("Enter the type of quota:");
-			select=scan.nextInt();
-			if(select<0)
-			{
-				System.out.println("Enter the type of quota:");
-				select=scan.nextInt();
-			}
-			switch(select)
-			{
-			case 1:
-				System.out.println("---Fees structure for Government quota---");
-				System.out.println("Fees structure for ECE is 4,00,000");
-				System.out.println("Fees structure for CSE is 4,50,000");
-				System.out.println("Fees structure for IT is  4,70,000");
-				System.out.println("Fees structure for MCA is 4,40,000");
-				break;
-			case 2:
-				System.out.println("---Fees structure for Management quota---");
-				System.out.println("Fees structure for ECE is 5,00,000");
-				System.out.println("Fees structure for CSE is 5,50,000");
-				System.out.println("Fees structure for IT is  5,60,000");
-				System.out.println("Fees structure for MCA is 5,70,000");
-				break;
-			}
-			break;
-		case 4:
-			System.out.println("---Welcome to KCT---");
-			System.out.println("College code is 5904");
-			System.out.println("1.Government quota\n 2.Management quota");
-			System.out.println("Enter the type of quota:");
-			select=scan.nextInt();
-			if(select<0)
-			{
-				System.out.println("Enter the type of quota:");
-				select=scan.nextInt();
-			}
-			switch(select)
-			{
-			case 1:
-				System.out.println("---Fees structure for Government quota---");
-				System.out.println("Fees structure for ECE is 4,00,000");
-				System.out.println("Fees structure for CSE is 4,50,000");
-				System.out.println("Fees structure for IT is  4,20,000");
-				System.out.println("Fees structure for MCA is 4,60,000");
-				break;
-			case 2:
-				System.out.println("---Fees structure for Management quota---");
-				System.out.println("Fees structure for ECE is 5,00,000");
-				System.out.println("Fees structure for CSE is 5,50,000");
-				System.out.println("Fees structure for IT is  5,20,000");
-				System.out.println("Fees structure for MCA is 5,60,000");
-				break;
-			}
-			break;
+				
 		}
 		System.out.println("Enter the college code:");
 		cp.collegeCode=scan.nextLong();
@@ -145,119 +26,8 @@ public class TestCollegeApp {
 			System.out.println("Enter the college code:");
 			cp.collegeCode=scan.nextLong();
 		}
-		System.out.println("Enter your cutoff:");
-		cp.cutOff=scan.nextInt();
-		if(cp.cutOff<0)
-		{
-			System.out.println("Enter your cutoff:");
-			cp.cutOff=scan.nextInt();
-		}
 		cp.setCollegeCode(cp.collegeCode);
-		cp.setCutOff(cp.cutOff);
 		ca.collegeDetails(cp.getCollegeCode());
-		ca.collegeDetails(cp.getCollegeCode(),cp.getCutOff());
-		/*cp.collegeName=scan.next();
-		String collegeNameCheck="^[a-zA-z]+$";
-		while(!cp.collegeName.matches(collegeNameCheck))
-		{
-			System.out.println("Enter the college name correctly:");
-			cp.collegeName=scan.next();
-		}
-		System.out.println("Enter the number of staffs working:");
-		cp.staff=scan.nextInt();
-		if(cp.staff<0)
-		{
-			System.out.println("Enter the number of staffs working again:");
-			cp.staff=scan.nextInt();
-		}
-		System.out.println("Enter the number of students pursuing:");
-		cp.student=scan.nextInt();
-		if(cp.student<0)
-		{
-			System.out.println("Enter the number of students pursuing again:");
-			cp.student=scan.nextInt();
-		}
-		System.out.println("Enter the number of departments:");
-		cp.department=scan.nextInt();
-		if(cp.department<0)
-		{
-			System.out.println("Enter the number of departments again:");
-			cp.department=scan.nextInt();
-		}
-		
-		System.out.println("Enter the number of buses:");
-		cp.bus=scan.nextInt();
-		if(cp.bus<0)
-		{
-			System.out.println("Enter the number of buses:");
-			cp.bus=scan.nextInt();
-		}
-		System.out.println("whether there is hostel facility are not(Y/N)?");
-		cp.hostel=scan.nextBoolean();
-		System.out.println("Enter the college code:");
-		cp.collegeCode=scan.nextLong();
-		if(cp.collegeCode<0)
-		{
-			System.out.println("Enter the college code:");
-			cp.collegeCode=scan.nextLong();
-		}
-		System.out.println("Enter the Branch code:");
-		cp.branchCode=scan.nextLong();
-		if(cp.branchCode<0)
-		{
-			System.out.println("Enter the Branch code:");
-			cp.branchCode=scan.nextLong();
-		}
-		cp.setCollegeName(cp.collegeName);
-		cp.setStaff(cp.staff);
-		cp.setStudent(cp.student);
-		cp.setDepartment(cp.department);
-		cp.setBus(cp.bus);
-		cp.setHostel(cp.hostel);
-		cp.setCollegeCode(cp.collegeCode);
-		cp.setBranchCode(cp.branchCode);
-		ca.collegeDetails(cp.getCollegeName(), cp.getStaff(), cp.getStudent());
-		ca.collegeDetails(cp.getDepartment(), cp.getBus());
-		ca.collegeDetails(cp.isHostel());
-		switch(cp.collegeName)
-		{
-		 case "PSNA":
-			 System.out.println("Total number of staffs:100");
-			 System.out.println("Total number of students:1000");
-			 System.out.println("Hostel facility is available here");
-			 break;
-		case "SSM":
-			 System.out.println("Total number of staffs:200");
-			 System.out.println("Total number of students:2000");
-			 System.out.println("Hostel facility is not available here");
-			 break;
-		case "KCT":
-			 System.out.println("Total number of staffs:300");
-			 System.out.println("Total number of students:3000");
-			 System.out.println("Hostel facility is available here");
-			 break;
-		default:
-				 System.out.println("wrong college");
-	}*/
-		/*System.out.println("1.Government quota\n 2.Management quota");
-		System.out.println("Enter the type of quota:");
-		int select=scan.nextInt();
-		switch(select)
-		{
-		case 1:
-			System.out.println("---Fees structure for Government quota---");
-			System.out.println("Fees structure for ECE is 1,000,00");
-			System.out.println("Fees structure for CSE is 1,500,00");
-			System.out.println("Fees structure for IT is 1,200,00");
-			System.out.println("Fees structure for MCA is 1,600,00");
-			break;
-		case 2:
-			System.out.println("Fees structure for ECE is 2,00,000");
-			System.out.println("Fees structure for CSE is 2,500,00");
-			System.out.println("Fees structure for IT is 2,200,00");
-			System.out.println("Fees structure for MCA is 2,600,00");
-			break;
-		}*/
 		scan.close();
 }
 }
